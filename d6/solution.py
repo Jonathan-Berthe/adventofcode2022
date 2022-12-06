c930a60d05007961fb3eeba52f485b48c6360652
Jonathan Berthe <jonathanb@apptweak.com>
2022-12-06 09:10:30 +0100

    Remove comment

--- d6/solution.py
+++ d6/solution.py
@@ -5,7 +5,7 @@ part = 2 # Variable: part = i => part i of exercise
 interval = 4 if part == 1 else 14
 solution = None
 for idx in range(0, len(line) - interval - 1):
-  substr = line[idx:idx + interval] # 4
+  substr = line[idx:idx + interval]
   if len(set(substr)) == len(substr):
     solution = idx + interval
     break
